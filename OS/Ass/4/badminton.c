e04da9b9bd14707a5965b673927c1ae91f4a9a54
zubairabid <zubairabid1999+github@gmail.com>
2018-10-22 07:33:02 +0530

    Started debugging 1

--- OS/Ass/4/badminton.c
+++ OS/Ass/4/badminton.c
@@ -6,21 +6,40 @@
 void *referee(void*);
 void *player(void*);
 
+void referee_startGame(int);
+void referee_enterCourt(int);
+void referee_enterAcademy(int);
+void referee_meetOrganizer(int);
+void referee_adjustEquipment(int);
+
+void player_warmUp(int);
+void player_enterCourt(int);
+void player_enterAcademy(int);
+void player_meetOrganizer(int);
+
 pthread_mutex_t organizer;
-pthread_cond_t cond;
+pthread_cond_t condp[20000];
+pthread_cond_t condr[10000];
+int locked = 0;
 
 int main() {
 
   // Initializing variables, not much to see here
   srand(time(0)); // Helper: setting a seed for rand
   int n, i, j, index;
-  int playercount, refcount;
+  int playercount, refcount, playat = 1, refat = 1;
   double playerprob;
   pthread_mutex_init(&organizer, NULL);
-  pthread_cond_init(&cond, NULL);
+  for(i = 0; i < 20000; i++) {
+    if (i < 10000) {
+      pthread_cond_init(&condr[i], NULL);
+    }
+    pthread_cond_init(&condp[i], NULL);
+  }
 
 
   // Take input as n, for number of referees and half the number of players
+  printf("Enter number n, given 3n people will come in total: \n");
   scanf("%d", &n);
   playercount = 2 * n;
   refcount = n;
@@ -35,6 +54,7 @@ int main() {
     // Creating a new player
     // As per question requirements
     playerprob = ((double)playercount/(playercount+refcount));
+    printf("Creating new player: playerprob = %f\n", playercount);
     if ( (playercount != 0) && ((refcount == 0) || (rand() <= playerprob)) ) {
       // New player is created
       index = 2*n - playercount + 1;
@@ -42,15 +62,37 @@ int main() {
       playercount--;
     } else {
       // New referee is created
-      pthread_create(&plr[n-refcount+1], NULL, referee, NULL);
+      index = n - refcount + 1;
+      pthread_create(&plr[index], NULL, referee, (void *)index);
       refcount--;
     }
 
     // Alloting groups, if organizer is free and people available
+    // pthread_create(&pint, NULL, allocator, NULL);
+    if (!locked) {
+      printf("Starting a game, organizer was unlocked\n");
+      pthread_mutex_lock(&organizer);
+      if ( ((2*n-playercount)-playat-1) < 2 || ((n-refcount)-refat-1) < 1 ) {
 
+        if(playat >= 2*n || refat >= n) {
+          break;
+        }
 
+        continue;
+      }
 
-  } while (true);
+      pthread_cond_signal(&condr[refat]);
+      pthread_cond_signal(&condp[playat]);
+      pthread_cond_signal(&condp[playat+1]);
+
+      playat += 2;
+      refat++;
+
+    }
+    printf("organizer was locked, continuing\n", );
+
+    sleep(1);
+  } while (1);
 
   return 0;
 }
@@ -59,62 +101,98 @@ void *referee(void *args) {
   int index = (int) args;
   // Enter the academy
   referee_enterAcademy(index);
+
   // Try to meet organizer (mutex)(print)
   // Meet organizer (lock rest out)(print)
+  pthread_mutex_lock(&organizer);
+  locked = 1;
+  referee_meetOrganizer(index);
+  locked = 0;
+  pthread_mutex_unlock(&organizer);
+
   // Wait till signal for new match (wait)
+  pthread_cond_wait(&condr[index], &organizer);
+
   // Lock organizer
   //  Enter court (print)
   referee_enterCourt(index);
+
   //  Adjust equipment (wait) (print)
+  referee_adjustEquipment(index);
+  sleep(0.5);
+
   //  Start match (rint)
+  referee_startGame(index);
+
   // Unlock organizer
+  pthread_mutex_unlock(&organizer);
 }
 
 void *player(void *args) {
   int index = (int) args;
   // Enter the academy
+  player_enterAcademy(index);
+
   // Try to meet organizer (mutex)(print)
   // Meet organizer (lock rest out)(print)
+  pthread_mutex_lock(&organizer);
+  locked = 1;
+  player_meetOrganizer(index);
+  locked = 0;
+  pthread_mutex_unlock(&organizer);
+
   // Wait till signal for new match (wait)
+  pthread_cond_wait(&condp[index], &organizer);
+
   // Lock organizer
   //  Enter court (print)
+  player_enterCourt(index);
+
   //  Warm up (wait) (print)
+  player_warmUp(index);
+  sleep(1);
   // Unlock organizer
+}
+
+
+
+void *allocator(void *arg) {
+
 }
 
 
 // Define player actions
 
 void player_meetOrganizer(int id) {
-  print("Player %d met the organizer", id);
+  printf("Player %d met the organizer", id);
 }
 void player_enterAcademy(int id) {
-  print("Player %d entered the Academy", id);
+  printf("Player %d entered the Academy", id);
 }
 void player_enterCourt(int id) {
-  print("Player %d entered the court", id);
+  printf("Player %d entered the court", id);
 }
 void player_warmUp(int id) {
-  print("Player %d is warming up", id);
+  printf("Player %d is warming up", id);
 }
 
 
 // Define referee actions
 
 void referee_meetOrganizer(int id) {
-  print("Referee %d met the organizer", id);
+  printf("Referee %d met the organizer", id);
 }
 void referee_enterAcademy(int id) {
-  print("Referee %d entered the Academy", id);
+  printf("Referee %d entered the Academy", id);
 }
 void referee_enterCourt(int id) {
-  print("Referee %d entered the court", id);
+  printf("Referee %d entered the court", id);
 }
 void referee_adjustEquipment(int id) {
-  print("Referee %d is adjusting equipment", id);
+  printf("Referee %d is adjusting equipment", id);
 }
 void referee_startGame(int id) {
-  print("Referee %d has started the game", id);
+  printf("Referee %d has started the game", id);
 }
 
 // Organizer: mutex
